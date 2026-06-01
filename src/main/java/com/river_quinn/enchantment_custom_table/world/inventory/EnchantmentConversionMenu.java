c180597dae1724fadaf453510718dba277df71d6
river_quinn <akitsukisaika153156@gmail.com>
2026-06-02 00:29:26 +0800

    fix: adapt conversion slot icons for 1.21.4

--- src/main/java/com/river_quinn/enchantment_custom_table/world/inventory/EnchantmentConversionMenu.java
+++ src/main/java/com/river_quinn/enchantment_custom_table/world/inventory/EnchantmentConversionMenu.java
@@ -218,11 +218,8 @@ public class EnchantmentConversionMenu extends AbstractContainerMenu {
 			}
 
 			@Override
-			public Pair<ResourceLocation, ResourceLocation> getNoItemIcon() {
-				return Pair.of(
-						InventoryMenu.BLOCK_ATLAS,
-						ResourceLocation.tryParse("enchantment_custom_table:item/empty_slot_book")
-				);
+			public ResourceLocation getNoItemIcon() {
+				return ResourceLocation.fromNamespaceAndPath("enchantment_custom_table", "container/slot/empty_slot_book");
 			}
 		});
 
@@ -241,11 +238,8 @@ public class EnchantmentConversionMenu extends AbstractContainerMenu {
 			}
 
 			@Override
-			public Pair<ResourceLocation, ResourceLocation> getNoItemIcon() {
-				return Pair.of(
-						InventoryMenu.BLOCK_ATLAS,
-						ResourceLocation.tryParse("enchantment_custom_table:item/empty_slot_book")
-				);
+			public ResourceLocation getNoItemIcon() {
+				return ResourceLocation.fromNamespaceAndPath("enchantment_custom_table", "container/slot/empty_slot_book");
 			}
 		});
 
