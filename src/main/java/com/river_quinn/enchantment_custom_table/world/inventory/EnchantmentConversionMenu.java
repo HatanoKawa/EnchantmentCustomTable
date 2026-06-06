4c0170b96c530ae1363745bd414c080451d39612
hatanokawa <hatanokawa@qq.com>
2026-06-07 02:58:58 +0800

    feat: add disabled icon and output icon, fix gui texture

--- src/main/java/com/river_quinn/enchantment_custom_table/world/inventory/EnchantmentConversionMenu.java
+++ src/main/java/com/river_quinn/enchantment_custom_table/world/inventory/EnchantmentConversionMenu.java
@@ -290,17 +290,17 @@ public class EnchantmentConversionMenu extends AbstractContainerMenu implements 
 			@Override
 			//? if >=1.21.11 {
 			public Identifier getNoItemIcon() {
-				return Identifier.fromNamespaceAndPath("enchantment_custom_table", "container/slot/empty_slot_book");
+				return Identifier.fromNamespaceAndPath("enchantment_custom_table", "container/slot/copy_template_slot");
 			}
 			//?} else if >=1.21.4 {
 			/*public ResourceLocation getNoItemIcon() {
-				return ResourceLocation.fromNamespaceAndPath("enchantment_custom_table", "container/slot/empty_slot_book");
+				return ResourceLocation.fromNamespaceAndPath("enchantment_custom_table", "container/slot/copy_template_slot");
 			}
 			*///?} else {
 			/*public Pair<ResourceLocation, ResourceLocation> getNoItemIcon() {
 				return Pair.of(
 						InventoryMenu.BLOCK_ATLAS,
-						ResourceLocation.tryParse("enchantment_custom_table:item/empty_slot_book")
+						ResourceLocation.tryParse("enchantment_custom_table:item/copy_template_slot")
 				);
 			}
 			*///?}
@@ -328,17 +328,17 @@ public class EnchantmentConversionMenu extends AbstractContainerMenu implements 
 			@Override
 			//? if >=1.21.11 {
 			public Identifier getNoItemIcon() {
-				return Identifier.fromNamespaceAndPath("enchantment_custom_table", "container/slot/empty_slot_book");
+				return Identifier.fromNamespaceAndPath("enchantment_custom_table", "container/slot/output_slot");
 			}
 			//?} else if >=1.21.4 {
 			/*public ResourceLocation getNoItemIcon() {
-				return ResourceLocation.fromNamespaceAndPath("enchantment_custom_table", "container/slot/empty_slot_book");
+				return ResourceLocation.fromNamespaceAndPath("enchantment_custom_table", "container/slot/output_slot");
 			}
 			*///?} else {
 			/*public Pair<ResourceLocation, ResourceLocation> getNoItemIcon() {
 				return Pair.of(
 						InventoryMenu.BLOCK_ATLAS,
-						ResourceLocation.tryParse("enchantment_custom_table:item/empty_slot_book")
+						ResourceLocation.tryParse("enchantment_custom_table:item/output_slot")
 				);
 			}
 			*///?}
