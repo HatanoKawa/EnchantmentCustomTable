4f24cc328907f0be04c2ec2a5aeff46b0c071713
river_quinn <akitsukisaika153156@gmail.com>
2026-07-19 22:13:00 +0800

    fix: lower 1.18.2 loader requirements

--- src/main/java/com/river_quinn/enchantment_custom_table/init/ModConfigScreens.java
+++ src/main/java/com/river_quinn/enchantment_custom_table/init/ModConfigScreens.java
@@ -1,13 +1,17 @@
 package com.river_quinn.enchantment_custom_table.init;
 
 import com.river_quinn.enchantment_custom_table.client.gui.TableConfigScreen;
-import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.client.ConfigGuiHandler;
+import net.minecraftforge.fml.ModLoadingContext;
 
 public final class ModConfigScreens {
     private ModConfigScreens() {
     }
 
     public static void register() {
-        MinecraftForge.registerConfigScreen(TableConfigScreen::new);
+        ModLoadingContext.get().registerExtensionPoint(
+                ConfigGuiHandler.ConfigGuiFactory.class,
+                () -> new ConfigGuiHandler.ConfigGuiFactory((minecraft, parent) -> new TableConfigScreen(parent))
+        );
     }
 }
